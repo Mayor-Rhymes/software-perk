a0aa0813c3677d73f9422935234679e0b3a7546f
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-08-25 13:55:37 +0100

    made changes to font

---
+++ src/lib/fonts.ts
@@ -0,0 +1,13 @@
+import { Alegreya, Inter } from "next/font/google";
+
+export const alegreya = Alegreya({
+  subsets: ["latin"],
+  variable: "--font-alegreya",
+  display: "swap",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
