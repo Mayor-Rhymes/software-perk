b41b38c5b2513fe3285e6f471c21ee4fbf32ca45
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-09-09 01:05:41 +0100

    implemented a hook to observe dom element for animation

---
+++ src/lib/hooks.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const useOnScreen = (ref: React.RefObject<HTMLElement>) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      },
+      { threshold: 0.3 }
+    );
+
+    const currentRef = ref.current;
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [ref]);
+
+  return isVisible;
+};
+
+export default useOnScreen;
