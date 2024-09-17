4ce877166ff04a6753838d19f9ab29be891707e6
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-09-17 23:46:18 +0100

    made change to servicecard

--- src/components/service-card.tsx
+++ src/components/service-card.tsx
@@ -1,11 +1,14 @@
-
-
-export default function ServiceCard({ title, description }: { title: string; description: string }) {
+export default function ServiceCard({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
   return (
     <div className="flex flex-col gap-6 bg-black text-white rounded-xl p-10 hover:shadow-lg transition-shadow">
       <h2 className="font-alegreya text-xl mb-3">{title}</h2>
       <p className="text-white">{description}</p>
-      <button className="w-full md:w-[50%] bg-white lg:w-[30%] rounded-md text-black py-2 text-sm">Learn more</button>
     </div>
   );
 }
