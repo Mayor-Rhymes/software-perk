ad329d4dc7b1f70bd00c1f4c92ca1cf6da72350b
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-10-27 13:28:57 +0000

    Removed sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,9 @@ import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { alegreya, inter } from '@/lib/fonts';
 import { Toaster } from '@/components/ui/sonner';
-
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import App from 'next/app';
+import { AppSidebar } from '@/components/app-sidebar';
 
 export const metadata: Metadata = {
   title: 'Software Perk',
@@ -23,11 +25,11 @@ export default function RootLayout({
         className={`bg-white ${inter.className} ${alegreya.className} font-sans`}
       >
         <Navbar />
-
         {children}
         <Footer />
         <Toaster position="top-right" />
       </body>
+ 
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import Designs from "@/components/designs";
-import Hero from "@/components/hero";
-import Showcase from "@/components/showcase";
-import Technologies from "@/components/technologies";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from '@/components/app-sidebar';
+import Designs from '@/components/designs';
+import Hero from '@/components/hero';
+import Showcase from '@/components/showcase';
+import Technologies from '@/components/technologies';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 export default function Home() {
   return (
-    <SidebarProvider>
     <main className="min-h-screen flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-10">
-      <SidebarTrigger />
       <Hero />
       <Showcase />
       <Designs />
       <Technologies />
     </main>
-    </SidebarProvider>
   );
 }
